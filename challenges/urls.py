52ed82b41436d3ae60235c88b70c8066a05e2f01
venkatmanavarthi <manavarthivenkat@icloud.com>
2021-08-11 17:36:38 +0530

    added dynamic paths

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     # dynamic path using angular brackets <>
+    path("", views.index),
     path("<int:month>", views.monthly_challenges_by_num),
-    path("<str:month>", views.monthly_challenges)
+    path("<str:month>", views.monthly_challenges, name='month-challenge')
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-
+from django.urls import reverse
 
 # data dict
 data_dict_by_month = {
@@ -12,18 +12,31 @@ data_dict_by_month = {
 # Create your views here.
 
 
+def index(request):
+
+    list_of_links = ""
+
+    for each_month in data_dict_by_month.keys():
+        path_for_each_month = reverse("month-challenge", args=[each_month])
+        list_of_links += f"<li><a href=\"{path_for_each_month}\">{each_month.upper()}</a></li>"
+
+    return HttpResponse("<ul>" + list_of_links + "</ul>")
+
+
 def monthly_challenges_by_num(request, month):
     try:
         months = list(data_dict_by_month.keys())
         redirect_month = months[month-1]
-        return HttpResponseRedirect("/challenges/" + redirect_month)
+        redirect_url = reverse("month-challenge", args=[redirect_month])
+        print(redirect_url)
+        return HttpResponseRedirect(redirect_url)
     except:
-        return HttpResponseNotFound("Not Found")
+        return HttpResponseNotFound("<h1>Not Found</h1>")
 
 
 def monthly_challenges(request, month):
     try:
         challenge_text = data_dict_by_month[month]
     except:
-        return HttpResponseNotFound("Month Not Found")
-    return HttpResponse(challenge_text)
+        return HttpResponseNotFound("<h2>Month Not Found</h2>")
+    return HttpResponse("<h1>" + challenge_text + "</h1>")
